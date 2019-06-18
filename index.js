167b80d2e1afcbf56b080582ff2bbd37e13fe21c
geonhwiii <g01063962671@gmail.com>
2019-06-18 16:33:40 +0900

    ex-complete

--- index.js
+++ index.js
@@ -2,23 +2,26 @@ const tweetButton = document.querySelector('.tweet-button');
 const refreshButton = document.querySelector('.refresh-button');
 const inputName = document.querySelector('.input-name');
 const inputMessage = document.querySelector('.input-message');
-const boxContainer = document.querySelector('.box-container')
+const boxContainer = document.querySelector('.box-container');
+const nullBox = document.querySelector('.null-box');
 
 let date = new Date();
 let now = date.format();
 
-function handleTweet() {
-  let iName = inputName.value;
-  let iMessage = inputMessage.value;
-  
-  const divBox = document.createElement('div');
+const NEW_DATA = DATA.slice();
 
+
+function makeBox(user, message, date) {
+  const divBox = document.createElement('div');
+  divBox.className = 'divBox'
   const nameBox = document.createElement('button');
-  nameBox.innerHTML = iName;
+  nameBox.addEventListener('click', handleFilter);
   const msgBox = document.createElement('p');
-  msgBox.innerHTML = iMessage;
   const dateBox = document.createElement('p');
-  dateBox.innerHTML = now;
+
+  nameBox.innerHTML = user;
+  msgBox.innerHTML = message;
+  dateBox.innerHTML = date;
 
   [nameBox, msgBox, dateBox].forEach(item => {
     divBox.appendChild(item);
@@ -27,13 +30,38 @@ function handleTweet() {
   boxContainer.appendChild(divBox);
 }
 
-function handleRefresh() {
-  alert('refresh');
+function handleTweet() {
+  let iName = inputName.value;
+  let iMessage = inputMessage.value;
+  makeBox(iName, iMessage, now)
+  let newObj = {user: iName, message: iMessage, created_at: now}
+  NEW_DATA.push(newObj);
 }
 
-function pageOnLoad() {
-  
+function handleRefresh() {
+  let randomData = generateNewTweet();
+  makeBox(randomData.user, randomData.message, now);
+  let newObj = {user: randomData.user, message: randomData.message, created_at: now}
+  NEW_DATA.push(newObj);
 }
+
+function handleFilter() {
+  boxContainer.innerHTML = '';
+  NEW_DATA
+    .filter(data => data.user === (this.textContent))
+    .forEach(data => {
+      makeBox(data.user, data.message, data.created_at)
+    })
+}
+
+
+function pageOnLoad() {
+  DATA.forEach(item => {
+    makeBox(item.user, item.message, item.created_at)
+  })
+}
+
+
 
 function init() {
   pageOnLoad();
